4b2fa6b7ab08465931d059a182787603a70c4347
Edgar Rincon <eers222@gmail.com>
2022-07-12 19:39:15 -0500

    Create newDeck function, and implement newDeck in main file

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace of diamonds"}
-	cards = append(cards, "Six of spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of diamonds"
 }
